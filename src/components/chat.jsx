6f9f04e79b9e551783f28713739402102c5aba9f
iiMaInUsErii <hromovartur94@gmail.com>
2023-10-20 18:06:46 +0300

    third

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,6 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-let msgList = []
+let listMsg = JSON.parse(localStorage.getItem("msg"))
+
+let infoMsg = {
+    user: (localStorage.getItem("username")), 
+    message:'',
+}
 
 const Chat = () => {
 
@@ -8,33 +13,62 @@ const Chat = () => {
 
     const [chat, setChat] = useState([])
 
+    useEffect(() => {
+        if(localStorage.getItem('msg') == null) {
+            localStorage.setItem('msg', JSON.stringify([]))
+            setChat(JSON.parse(localStorage.getItem('msg')))
+        } else {
+            setChat(JSON.parse(localStorage.getItem('msg')))
+        }
+    }, [])
+
     return (
         <>
             <div className="chat">
-                {chat.join('\n')}
+                {chat.map((el, key) => (
+                    <div className="message">
+                        <div className="user">
+                            { el.user }:
+                        </div>
+                        <div className="message">
+                            { el.message }
+                            <button
+                            onClick={() => {
+                    listMsg.splice(key, 1)
+                    localStorage.setItem("msg", JSON.stringify(listMsg))
+                    listMsg = JSON.parse(localStorage.getItem("msg"))
+                    infoMsg.message = ""
+                    setChat(JSON.parse(localStorage.getItem('msg')))
+                            }}>Delete</button>
+                        </div>
+                    </div>
+                ))}
             </div>
 
-            <div>
-                <label>New message</label>
+            <div class="inputMessage">
                 <input
+                    type="text"
+                    placeholder="Введите сообщение"
                     value={message}
                     onChange={(event) => {
                         setMessage(event.target.value)
                     }}
                 />
-                <button
-                    onClick={() => {
-                        msgList.push((localStorage.getItem('username') + ": " + message))
-                        setChat(msgList)
-                        setMessage('')
-                    }}
-                >Send</button>
-                <button
-                    onClick={() => {
-                        msgList.pop()
-                        setChat(msgList)
-                    }}
-                >Delete</button>
+                <button onClick={() => {
+                    infoMsg.message = message
+                    listMsg.push(infoMsg)
+                    localStorage.setItem("msg", JSON.stringify(listMsg))
+                    listMsg = JSON.parse(localStorage.getItem("msg"))
+                    infoMsg.message = ""
+                    setChat(JSON.parse(localStorage.getItem('msg')))
+                }}>Отправить</button>
+                <button onClick={() => {
+                    listMsg.pop()
+                    localStorage.setItem("msg", JSON.stringify(listMsg))
+                    listMsg = JSON.parse(localStorage.getItem("msg"))
+                    infoMsg.message = ""
+                    setChat(JSON.parse(localStorage.getItem('msg')))
+                }}>Удалить</button>
             </div>
         </>
     )
